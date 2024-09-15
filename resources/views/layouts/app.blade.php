e8fa4e199602567b35034987a4a12402962946f4
phuwis <phuwis.tweed@gmail.com>
2024-09-15 09:09:35 +0700

    fix: 🐛 new-template
    BREAKING CHANGE: 🧨 added-form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,11 +83,7 @@
     </div>
 
     <!-- Footer -->
-    <footer class="text-center py-4 mt-auto" style="background-color: #343a40; color: white;">
-      <div class="container">
-        <p class="mb-0">&copy; 2024 My Blog. All Rights Reserved.</p>
-      </div>
-    </footer>
+
   </div>
 </body>
 
